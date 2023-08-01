052b1d1122e63b03178126c6cfd3cf9d281af56e
Shakerul Islam Sayem <shakerulislam987@gmail.com>
2023-08-01 11:40:59 +0600

    Application Setup

---
+++ resources/views/admin/products.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin')
+@section('title')
+    {{ 'Dashboard' }}
+@endsection
+@section('content')
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <p class="mb-0">This is a sample page </p>
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi minima sit officia, doloremque quidem reiciendis ullam quas beatae sequi et, quos cum ab quaerat at voluptatem magnam molestiae quasi non?</h1>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/favicon.png') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}" />
+
+
+</head>
+
+<body>
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        @include('partials.admin.sidebar')
+        <!--  Main wrapper -->
+        <div class="body-wrapper">
+            @include('partials.admin.navbar')
+            @yield('content')
+
+        </div>
+    </div>
+    <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/sidebarmenu.js') }}"></script>
+    <script src="{{ asset('assets/js/app.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/dist/simplebar.js') }}"></script>
+    <script src="{{ asset('assets/fontawesome/js/all.min.js') }}"></script>
+</body>
+
+</html>
