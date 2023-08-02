65f078b2ec527442f4e81ff29a853d309c1968aa
Shakerul Islam Sayem <shakerulislam987@gmail.com>
2023-08-02 12:40:39 +0600

    update

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -6,8 +6,45 @@
     <div class="container-fluid">
         <div class="card">
             <div class="card-body">
-                <p class="mb-0">This is a sample page </p>
-                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi minima sit officia, doloremque quidem reiciendis ullam quas beatae sequi et, quos cum ab quaerat at voluptatem magnam molestiae quasi non?</h1>
+                <div class="container mt-4">
+                    <h2>Add Product</h2>
+                    <form>
+                        <div class="mb-3">
+                            <label for="product-name" class="form-label">Product Name</label>
+                            <input type="text" class="form-control" id="product-name" name="product-name" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="product-description" class="form-label">Product Description</label>
+                            <textarea class="form-control" id="product-description" name="product-description" rows="3" required></textarea>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="product-price" class="form-label">Product Price</label>
+                            <input type="number" class="form-control" id="product-price" name="product-price"
+                                step="0.01" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="product-quantity" class="form-label">Product Quantity</label>
+                            <input type="number" class="form-control" id="product-quantity" name="product-quantity"
+                                required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="product-category" class="form-label">Product Category</label>
+                            <select class="form-control" id="product-category" name="product-category" required>
+                                <option value="" disabled selected>Select a category</option>
+                                <option value="category1">Category 1</option>
+                                <option value="category2">Category 2</option>
+                                <option value="category3">Category 3</option>
+                            </select>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Add Product</button>
+                        
+                    </form>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/partials/admin/Sidebar.blade.php
+++ resources/views/partials/admin/Sidebar.blade.php
@@ -1,129 +1,154 @@
-    <!-- Sidebar Start -->
-    <aside class="left-sidebar">
-      <!-- Sidebar scroll-->
-      <div>
+<aside class="left-sidebar">
+    <div>
         <div class="brand-logo d-flex align-items-center justify-content-between">
-          <a href="./index.html" class="text-nowrap logo-img">
-            <img src="{{asset('assets/images/logos/dark-logo.svg')}}" width="180" alt="" />
-          </a>
-          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-            <i class="ti ti-x fs-8"></i>
-          </div>
+            <a href="./index.html" class="text-nowrap logo-img">
+                <img src="{{ asset('assets/images/logos/dark-logo.svg') }}" width="180" alt="" />
+            </a>
+            <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                <i class="ti ti-x fs-8"></i>
+            </div>
         </div>
-        <!-- Sidebar navigation-->
         <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-          <ul id="sidebarnav">
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">Home</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./index.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-layout-dashboard"></i>
-                </span>
-                <span class="hide-menu">Dashboard</span>
-              </a>
-            </li>
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">UI COMPONENTS</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-buttons.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-article"></i>
-                </span>
-                <span class="hide-menu">Buttons</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-alert-circle"></i>
-                </span>
-                <span class="hide-menu">Alerts</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-card.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-cards"></i>
-                </span>
-                <span class="hide-menu">Card</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-forms.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-file-description"></i>
-                </span>
-                <span class="hide-menu">Forms</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-typography.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-typography"></i>
-                </span>
-                <span class="hide-menu">Typography</span>
-              </a>
-            </li>
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">AUTH</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-login"></i>
-                </span>
-                <span class="hide-menu">Login</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-register.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-user-plus"></i>
-                </span>
-                <span class="hide-menu">Register</span>
-              </a>
-            </li>
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">EXTRA</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./icon-tabler.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-mood-happy"></i>
-                </span>
-                <span class="hide-menu">Icons</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./sample-page.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-aperture"></i>
-                </span>
-                <span class="hide-menu">Sample Page</span>
-              </a>
-            </li>
-          </ul>
-          <!-- <div class="unlimited-access hide-menu bg-light-primary position-relative mb-7 mt-5 rounded">
-              <div class="d-flex">
-                <div class="unlimited-access-title me-3">
-                  <h6 class="fw-semibold fs-4 mb-6 text-dark w-85">Upgrade to pro</h6>
-                  <a href="https://adminmart.com/product/modernize-bootstrap-5-admin-template/" target="_blank" class="btn btn-primary fs-2 fw-semibold lh-sm">Buy Pro</a>
-                </div>
-                <div class="unlimited-access-img">
-                  <img src="{{asset('assets/images/backgrounds/rocket.png')}}" alt="" class="img-fluid">
-                </div>
-              </div>
-            </div> -->
+            <ul id="sidebarnav">
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">Home</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./index.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-layout-dashboard"></i>
+                        </span>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">INVENTORY</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./ui-buttons.html" class="nav-link dropdown-toggle"
+                        href="#navbarVerticalMenuDashboards" role="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarVerticalMenuDashboards" aria-expanded="true"
+                        aria-controls="navbarVerticalMenuDashboards" style="backgraound-color: rgb(212, 83, 83);">
+                        <span>
+                            <i class="ti ti-article"></i>
+                        </span>
+                        <span class="hide-menu">Inward</span>
+                    </a>
+                    <div id="navbarVerticalMenuDashboards" class="nav-collapse collapse"
+                        data-bs-parent="#navbarVerticalMenu" hs-parent-area="#navbarVerticalMenu" style="margin-left: 1rem; backgraound-color: rgb(206, 33, 33);">
+                        <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
+                            <span>
+                                <i class="ti ti-alert-circle"></i>
+                            </span>
+                            <span class="hide-menu">1. Outward</span>
+                        </a>
+                        <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
+                            <span>
+                                <i class="ti ti-alert-circle"></i>
+                            </span>
+                            <span class="hide-menu">2. Outward</span>
+                        </a>
+                    </div>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-alert-circle"></i>
+                        </span>
+                        <span class="hide-menu">Outward</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./ui-typography.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-typography"></i>
+                        </span>
+                        <span class="hide-menu">Stock</span>
+                    </a>
+                </li>
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">PRODUCTS</span>
+                </li>
+                <li class="sidebar-item submenu">
+                    <a class="sidebar-link" href="./ui-card.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-cards"></i>
+                        </span>
+                        <span class="hide-menu">Product List</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./ui-forms.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-file-description"></i>
+                        </span>
+                        <span class="hide-menu">Category</span>
+                    </a>
+                </li>
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">SUPPLIER</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./ui-typography.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-typography"></i>
+                        </span>
+                        <span class="hide-menu">Supplier Profile</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./ui-typography.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-typography"></i>
+                        </span>
+                        <span class="hide-menu">Supplier</span>
+                    </a>
+                </li>
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">AUTH</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-login"></i>
+                        </span>
+                        <span class="hide-menu">Login</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./authentication-register.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-user-plus"></i>
+                        </span>
+                        <span class="hide-menu">Register</span>
+                    </a>
+                </li>
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">EXTRA</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./icon-tabler.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-mood-happy"></i>
+                        </span>
+                        <span class="hide-menu">Icons</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="./sample-page.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-aperture"></i>
+                        </span>
+                        <span class="hide-menu">Sample Page</span>
+                    </a>
+                </li>
+            </ul>
         </nav>
-        <!-- End Sidebar navigation -->
-      </div>
-      <!-- End Sidebar scroll-->
-    </aside>
-    <!--  Sidebar End -->
+    </div>
+</aside>
